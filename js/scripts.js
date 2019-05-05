edcf57dd63a491c5471453a53d6c9ee79e0c38f9
muhindokiro <blaize1143@gmail.com>
2019-05-05 17:29:09 +0300

    add style to display text when design icon is clicked

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+$(".clickable").click(function(){
+  $(".design-hidden").toggle();
+  $(".design-showing").toggle();
+});
+$(".clickable2").click(function(){
+  $(".development-hidden").toggle();
+  $(".development-showing").toggle();
+});
+$(".clickable3").click(function(){
+  $(".management-hidden").toggle();
+  $(".management-showing").toggle();
+});
+});
